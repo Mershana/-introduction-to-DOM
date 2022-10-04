d16f298410d4fab73bfc4ef51675e10012a30b74
Mershana <kriomaas@gmail.com>
2022-10-04 19:22:44 +0300

    05. Именованные обработчики событий

---
+++ 05. Именованные обработчики событий/js/app.js
@@ -0,0 +1,31 @@
+'use strict'
+
+// Именованные обработчики событий в JavaScript
+/* №1⊗jsPmDmNEH
+Дан следующий HTML код:
+
+<input id="button1" type="submit" value="button1">
+<input id="button2" type="submit" value="button2">
+Даны следующие функции:
+
+function func1() {
+	console.log(1);
+}
+
+function func2() {
+	console.log(2);
+}
+Сделайте так, чтобы по клику на первую кнопку выполнилась функция func1, а по клику на вторую - функция func2. */
+
+let button1 = document.querySelector('#button1');
+let button2 = document.querySelector('#button2');
+button1.addEventListener('click', func1)
+button2.addEventListener('click', func2)
+
+function func1(){
+	console.log('сработала функция 1');
+}
+
+function func2(){
+	console.log('сработала функция 2');
+}
