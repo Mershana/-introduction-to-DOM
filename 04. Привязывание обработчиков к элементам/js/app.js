3b88fa289d148d012c7171b6ffdd4d7f794e3785
Mershana <kriomaas@gmail.com>
2022-10-04 19:22:32 +0300

    04. Привязывание обработчиков к элементам

---
+++ 04. Привязывание обработчиков к элементам/js/app.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// Привязывание обработчиков к элементам в JavaScript
+/* №1⊗jsPmDmEHB
+Даны 3 кнопки:
+<input id="button1" type="submit">
+<input id="button2" type="submit">
+<input id="button3" type="submit">
+Сделайте так, чтобы по клику на первую кнопку на экран выводилось число 1, по клику на вторую - число 2, а по клику на третью - число 3. */
+/* let button1 = document.querySelector('#button1');
+let button2 = document.querySelector('#button2');
+let button3 = document.querySelector('#button3');
+
+button1.addEventListener('click', function(){
+	console.log('button1');
+})
+button2.addEventListener('click', function(){
+	console.log('button2');
+})
+button3.addEventListener('click', function(){
+	console.log('button3');
+}) */
