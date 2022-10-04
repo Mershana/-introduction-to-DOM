79092c70d29567ffca1a6490c32182177a309ef8
Mershana <kriomaas@gmail.com>
2022-10-04 19:22:56 +0300

    06. Один обработчик ко многим элементам

---
+++ 06. Один обработчик ко многим элементам/js/app.js
@@ -0,0 +1,33 @@
+'use strict'
+
+// Один обработчик ко многим элементам в JavaScript
+/* №1⊗jsPmDmMEH
+Даны 5 абзацев:
+
+<p id="elem1">text</p>
+<p id="elem2">text</p>
+<p id="elem3">text</p>
+<p id="elem4">text</p>
+<p id="elem5">text</p>
+Дана следующая функция:
+
+function func() {
+	console.log('message');
+}
+Привяжите эту функцию ко всем 5-ти абзацам. */
+
+let elem1 = document.querySelector('#elem1');
+let elem2 = document.querySelector('#elem2');
+let elem3 = document.querySelector('#elem3');
+let elem4 = document.querySelector('#elem4');
+let elem5 = document.querySelector('#elem5');
+
+elem1.addEventListener('click', func)
+elem2.addEventListener('click', func)
+elem3.addEventListener('click', func)
+elem4.addEventListener('click', func)
+elem5.addEventListener('click', func)
+
+function func() {
+	console.log('message');
+}
