333d82148733c5e255171c03f24742d30a38ef07
Mershana <kriomaas@gmail.com>
2022-10-12 19:13:47 +0300

    022. Отвязывание обработчиков событий в цикле

---
+++ 022. Отвязывание обработчиков событий в цикле/js/app.js
@@ -0,0 +1,16 @@
+'use strict'
+// Отвязывание обработчиков событий в цикле в JavaScript
+
+/* №1⊗jsPmDmLHU
+Даны абзацы. По нажатию на любой из абзацев добавьте ему в конец восклицательный знак. Сделайте так, чтобы это добавление происходило лишь по первому нажатию. */
+let elems = document.querySelectorAll('p');
+
+for (let elem of elems){
+	elem.addEventListener('click', func);
+}
+
+function func(){
+	this.textContent += '!';
+	this.removeEventListener('click', func);
+}
+
