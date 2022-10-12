fe75e6f962ebc973301f452e4c5b8c46c0568e91
Mershana <kriomaas@gmail.com>
2022-10-12 19:12:20 +0300

    015. Цепочки методов и свойств

---
+++ 015. Цепочки методов и свойств/js/app.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// Цепочки методов и свойств в JavaScript
+
+/* №1⊗jsPmDmChn
+Дан следующий код:
+<img id="image" src="avatar.png">
+let image = document.querySelector('#image');
+console.log(image.src);
+Переделайте приведенный выше код так, чтобы вместо введения переменной image использовалась цепочка. */
+/* 
+console.log(document.querySelector('#image').src); */
+
+// Преимущества и недостатки цепочек
+/* №2⊗jsPmDmChn
+Дан следующий код:
+<img id="image" src="avatar.png" width="300" height="500">
+console.log(document.querySelector('#image').src);
+console.log(document.querySelector('#image').width);
+console.log(document.querySelector('#image').height);
+Укажите на недостатки данного кода. Исправьте их. */
+/* let image = document.querySelector('#image');
+console.log(image.src);
+console.log(image.width);
+console.log(image.height); */
