cf10693dc7246574cb59d69f7128fe1293737c4a
Mershana <kriomaas@gmail.com>
2022-10-14 19:27:48 +0300

    030. Поиск элементов внутри другого элемента

---
+++ 030. Поиск элементов внутри другого элемента/js/app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// Поиск элементов внутри другого элемента в JavaScript
+/* №1⊗jsPmDmIES
+Дан элемент #parent:
+<div id="parent">
+	<p class="www">text</p>
+	<p class="www">text</p>
+	<p class="www">text</p>
+	<p class="ggg">text</p>
+	<p class="ggg">text</p>
+	<p class="ggg">text</p>
+</div>
+let parent = document.querySelector('#parent');
+Найдите внутри родителя элементы с классом www и запишите их в переменную elems1. Затем найдите внутри родителя элементы с классом ggg и запишите их в переменную elems2. */
+/* let parent = document.querySelector('#parent');
+let elems1 = parent.querySelectorAll('.www');
+let elems2 = parent.querySelectorAll('.ggg');
+console.log(elems1);
+console.log(elems2); */
